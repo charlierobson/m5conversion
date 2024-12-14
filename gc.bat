d86b667b6f0588b071d64c954397e62eb8b911ac
charlie robson <charlie_robson@hotmail.com>
2024-12-14 11:32:06 +0000

    cleanup before m4

--- gc.bat
+++ gc.bat
@@ -1,4 +1,8 @@
+rem tracereg,tracemode,remap67,remapIn,remapOut,remapAll,logVDP
+set m5opts="tracereg"
+
 pushd ..\mamebuild
-start mame coleco -debug -nomaximize -uimodekey HOME -debugscript ..\m5conversion\dbscript.txt %1%
+rem start mame coleco -debug -nomaximize -uimodekey HOME -debugscript ..\m5conversion\dbscript.txt %1
+start mame coleco -debug -nomaximize -uimodekey HOME -m5c %m5opts%  %1
 popd
 exit /b
